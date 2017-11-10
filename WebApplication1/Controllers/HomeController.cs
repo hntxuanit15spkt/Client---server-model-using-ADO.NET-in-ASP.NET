00b034b1361d52b8d23ab2d9b2fb95ee21726ebe
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2017-11-10 23:05:53 +0700

    Xuan

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Helper;
 
 namespace WebApplication1.Controllers
 {
@@ -12,7 +14,29 @@ namespace WebApplication1.Controllers
     {
       return View();
     }
-
+    public ActionResult Show(string ip, string Databasename, string usr, string pwd)
+    {
+      Connect.ConnectString = " Data Source=" + ip + ";Initial Catalog=" + Databasename + ";Integrated Security=False;User ID=" + usr + ";Password=" + pwd;
+      Config cf = new Config(Connect.ConnectString);
+      //var list = new List<SanPham>();
+      var check = cf.Connection();
+      if (check)
+      {
+        TempData["result"] = "Kết nối thành công!";
+        //list = cf.ListAll("select * from SanPham where DaXoa=0");
+      }
+      else
+      {
+        TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
+        return RedirectToAction("Index", "test");
+      }
+      return View();
+    }
+    public ActionResult DisConnect()
+    {
+      Connect.ConnectString = "";
+      return RedirectToAction("Index", "Home");
+    }
     public ActionResult About()
     {
       ViewBag.Message = "Your application description page.";
---
+++ WebApplication1/Controllers/SanPhamController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+using WebApplication1.Helper;
+
+namespace WebApplication1.Controllers
+{
+  public class SanPhamController : Controller
+  {
+    // GET: Test
+
+    public SanPhamController()
+    {
+    }
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public ActionResult Delete(int id)
+    {
+      Config cf = new Config(Connect.ConnectString);
+      var list = new List<SanPham>();
+      var check = cf.Connection();
+      if (check)
+      {
+        TempData["results"] = "Xóa thành công!";
+        cf.Delete("Delete SanPham where id = " + id);
+
+      }
+      else
+      {
+        TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
+        return RedirectToAction("Index", "test");
+      }
+      return RedirectToAction("Show", "test");
+    }
+    public ActionResult Update(SanPham sp)
+    {
+      Config cf = new Config(Connect.ConnectString);
+      var list = new List<SanPham>();
+      var check = cf.Connection();
+      if (check)
+      {
+        sp.TenSP = "Bình Trôi";
+        TempData["results"] = "Sửa thành công!";
+        string sql = "Update  SanPham  set TenSP=N'" + sp.TenSP + "' where MaSP=" + sp.MaSP;
+        cf.Update(sql);
+      }
+      else
+      {
+        TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
+        return RedirectToAction("Index", "test");
+      }
+      return RedirectToAction("Show", "test");
+    }
+    [HttpGet]
+    public ActionResult Insert()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Insert(string TenSP, decimal DonGia, string Mota, string Hinhanh,
+      int SoLuongton, int LuotXem, int LuotBinhChon, int MaLoaiSP, bool DaXoa, int MaSukien)
+    {
+      //public ActionResult Insert(string TenSP)
+      //{
+      Config cf = new Config(Connect.ConnectString);
+      var list = new List<SanPham>();
+      var check = cf.Connection();
+      if (check)
+      {
+        TempData["results"] = "Thêm thành công!";
+        cf.Insert("INSERT INTO SanPham VALUES(" + TenSP + ", " 
+          + DonGia + "," + Mota + "," + Hinhanh + "," + SoLuongton + "," + LuotXem + "," + LuotBinhChon
+          + "," + MaLoaiSP + "," + DaXoa + "," + MaSukien + ");");
+        //cf.Insert("INSERT INTO SanPham VALUES(" + TenSP + ");");
+      }
+      else
+      {
+        TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
+        return RedirectToAction("Index", "test");
+      }
+      return RedirectToAction("Show", "test");
+    }
+    
+  }
+
+}
