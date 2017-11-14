5a710a2e6f3a9a6282c9ccd2ab0c202e0dd7aea9
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2017-11-14 14:19:34 +0700

    a

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,77 +10,77 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+
+    public ActionResult Index()
     {
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Show(string ip, string Databasename, string usr, string pwd)
-        {
-            Connect.ConnectString = " Data Source=" + ip + ";Initial Catalog=" + Databasename + ";Integrated Security=False;User ID=" + usr + ";Password=" + pwd;
-            // Config cf = new Config(Connect.ConnectString);
-            //var list = new List<SanPham>();
-            //var check = cf.Connection();
-            if (Connect.CheckConnection())
-            {
-                TempData["result"] = "Kết nối thành công!";
-                //list = cf.ListAll("select * from SanPham where DaXoa=0");
-            }
-            else
-            {
-                TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
-                return RedirectToAction("Index", "test");
-            }
-            return View();
-        }
-        public ActionResult DisConnect()
-        {
-            Connect.ConnectString = "";
-            return RedirectToAction("Index", "Home");
-        }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-        public ActionResult DangKy()
-        {
-            return View();
-        }
-        public ActionResult MenuPartial()
-        {
-            //var lstSP = db.SanPhams;
-            Config cf = new Config(Connect.ConnectString);
-            List<LOAISANPHAM> listLSP = new List<LOAISANPHAM>();
-            LOAISANPHAM lsp = null;
-            DataTable dtSP = cf.ExecuteQuery("select * from LOAISANPHAM");
-            foreach (DataRow item in dtSP.Rows)
-            {
-                lsp = new LOAISANPHAM(item);
-                listLSP.Add(lsp);
-            }
-            return PartialView(listLSP);
-        }
-        //public ActionResult MenuPartial()
-        //{
-        //    //var lstSP = db.SanPhams;
-        //    Config cf = new Config(Connect.ConnectString);
-        //    List<SANPHAM> listSP = new List<SANPHAM>();
-        //    DataTable dtSP = cf.ExecuteQuery("select * from SanPham");
-        //    foreach (DataRow item in dtSP.Rows)
-        //    {
-        //        SANPHAM sp = new SANPHAM(item);
-        //        listSP.Add(sp);
-        //    }
-        //    return PartialView(listSP);
-        //}
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            return View();
-        }
+      return View();
     }
+    public ActionResult Show(string ip, string Databasename, string usr, string pwd)
+    {
+      Connect.ConnectString = " Data Source=" + ip + ";Initial Catalog=" + Databasename + ";Integrated Security=False;User ID=" + usr + ";Password=" + pwd;
+      // Config cf = new Config(Connect.ConnectString);
+      //var list = new List<SanPham>();
+      //var check = cf.Connection();
+      if (Connect.CheckConnection())
+      {
+        TempData["result"] = "Kết nối thành công!";
+        //list = cf.ListAll("select * from SanPham where DaXoa=0");
+      }
+      else
+      {
+        TempData["result"] = "Kết nối cơ sở dữ liệu không thành công!";
+        return RedirectToAction("Index", "Home");
+      }
+      return View();
+    }
+    public ActionResult DisConnect()
+    {
+      Connect.ConnectString = "";
+      return RedirectToAction("Index", "Home");
+    }
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+    public ActionResult DangKy()
+    {
+      return View();
+    }
+    public ActionResult MenuPartial()
+    {
+      //var lstSP = db.SanPhams;
+      Config cf = new Config(Connect.ConnectString);
+      List<LOAISANPHAM> listLSP = new List<LOAISANPHAM>();
+      LOAISANPHAM lsp = null;
+      DataTable dtSP = cf.ExecuteQuery("select * from LOAISANPHAM");
+      foreach (DataRow item in dtSP.Rows)
+      {
+        lsp = new LOAISANPHAM(item);
+        listLSP.Add(lsp);
+      }
+      return PartialView(listLSP);
+    }
+    //public ActionResult MenuPartial()
+    //{
+    //    //var lstSP = db.SanPhams;
+    //    Config cf = new Config(Connect.ConnectString);
+    //    List<SANPHAM> listSP = new List<SANPHAM>();
+    //    DataTable dtSP = cf.ExecuteQuery("select * from SanPham");
+    //    foreach (DataRow item in dtSP.Rows)
+    //    {
+    //        SANPHAM sp = new SANPHAM(item);
+    //        listSP.Add(sp);
+    //    }
+    //    return PartialView(listSP);
+    //}
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+      return View();
+    }
+  }
 }
--- WebApplication1/Models/SanPham.cs
+++ WebApplication1/Models/SanPham.cs
@@ -7,50 +7,54 @@ using System.Web;
 
 namespace WebApplication1.Models
 {
-    public class SANPHAM
+  public class SANPHAM
+  {
+    public SANPHAM()
     {
-        public SANPHAM(DataRow red)
-        {
-            this.MaSP = Convert.ToInt32(red["MaSP"]);
-            if (!Convert.IsDBNull(red["TenSP"]))
-                this.TenSP = red["TenSP"].ToString();
-            if (!Convert.IsDBNull(red["NgayCapNhat"]))
-                this.NgayCapNhat = Convert.ToDateTime(red["NgayCapNhat"]);
-            if (!Convert.IsDBNull(red["DonGia"]))
-                this.DonGia = Convert.ToDecimal(red["DonGia"]);
-            this.Mota = red["MoTa"].ToString();
-            this.HinhAnh = red["HinhAnh"].ToString();
-            if (!Convert.IsDBNull(red["SoLuongTon"]))
-                this.SoLuongton = Convert.ToInt32(red["SoLuongTon"]);
-            if (!Convert.IsDBNull(red["LuotXem"]))
-                this.LuotXem = Convert.ToInt32(red["LuotXem"]);
-            if (!Convert.IsDBNull(red["LuotBinhChon"]))
-                this.LuotBinhChon = Convert.ToInt32(red["LuotBinhChon"]);
-            if (!Convert.IsDBNull(red["MaLoaiSP"]))
-                this.MaLoaiSP = Convert.ToInt32(red["MaLoaiSP"]);
-            if (!Convert.IsDBNull(red["DaXoa"]))
-                this.DaXoa = Convert.ToBoolean(red["DaXoa"]);
-            if (!Convert.IsDBNull(red["NgayDang"]))
-                this.NgayDang = Convert.ToDateTime(red["NgayDang"]);
-            if (!Convert.IsDBNull(red["MaSuKien"]))
-                this.MaSukien = Convert.ToInt32(red["MaSuKien"]);
-        }
-        public int MaSP { get; set; }
-        [StringLength(300)]
-        public string TenSP { get; set; }
-        public DateTime NgayCapNhat { get; set; }
-        public decimal DonGia { get; set; }
-        public string Mota { get; set; }
-        public string HinhAnh { get; set; }
-        public int SoLuongton { get; set; }
-        public int LuotXem { get; set; }
-        public int LuotBinhChon { get; set; }
-        public int MaLoaiSP { get; set; }
-        public bool DaXoa { get; set; }
-        public DateTime NgayDang { get; set; }
-        public int MaSukien { get; set; }
-        public virtual LOAISANPHAM LOAISANPHAM { get; set; }
-        public virtual SUKIEN SUKIEN { get; set; }
-        public virtual ICollection<CHITIETPHIEUNHAP> CHITIETPHIEUNHAPs { get; set; }
+
     }
+    public SANPHAM(DataRow red)
+    {
+      this.MaSP = Convert.ToInt32(red["MaSP"]);
+      if (!Convert.IsDBNull(red["TenSP"]))
+        this.TenSP = red["TenSP"].ToString();
+      if (!Convert.IsDBNull(red["NgayCapNhat"]))
+        this.NgayCapNhat = Convert.ToDateTime(red["NgayCapNhat"]);
+      if (!Convert.IsDBNull(red["DonGia"]))
+        this.DonGia = Convert.ToDecimal(red["DonGia"]);
+      this.MoTa = red["MoTa"].ToString();
+      this.HinhAnh = red["HinhAnh"].ToString();
+      if (!Convert.IsDBNull(red["SoLuongTon"]))
+        this.SoLuongTon = Convert.ToInt32(red["SoLuongTon"]);
+      if (!Convert.IsDBNull(red["LuotXem"]))
+        this.LuotXem = Convert.ToInt32(red["LuotXem"]);
+      if (!Convert.IsDBNull(red["LuotBinhChon"]))
+        this.LuotBinhChon = Convert.ToInt32(red["LuotBinhChon"]);
+      if (!Convert.IsDBNull(red["MaLoaiSP"]))
+        this.MaLoaiSP = Convert.ToInt32(red["MaLoaiSP"]);
+      if (!Convert.IsDBNull(red["DaXoa"]))
+        this.DaXoa = Convert.ToBoolean(red["DaXoa"]);
+      if (!Convert.IsDBNull(red["NgayDang"]))
+        this.NgayDang = Convert.ToDateTime(red["NgayDang"]);
+      if (!Convert.IsDBNull(red["MaSuKien"]))
+        this.MaSuKien = Convert.ToInt32(red["MaSuKien"]);
+    }
+    public int MaSP { get; set; }
+    [StringLength(300)]
+    public string TenSP { get; set; }
+    public DateTime NgayCapNhat { get; set; }
+    public decimal DonGia { get; set; }
+    public string MoTa { get; set; }
+    public string HinhAnh { get; set; }
+    public int SoLuongTon { get; set; }
+    public int LuotXem { get; set; }
+    public int LuotBinhChon { get; set; }
+    public int MaLoaiSP { get; set; }
+    public bool DaXoa { get; set; }
+    public DateTime NgayDang { get; set; }
+    public int MaSuKien { get; set; }
+    public virtual LOAISANPHAM LOAISANPHAM { get; set; }
+    public virtual SUKIEN SUKIEN { get; set; }
+    public virtual ICollection<CHITIETPHIEUNHAP> CHITIETPHIEUNHAPs { get; set; }
+  }
 }
