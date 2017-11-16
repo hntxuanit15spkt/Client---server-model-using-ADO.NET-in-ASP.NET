d5ac31ee00275901b40eb51f77b1fc03c18e1145
hntxuanit15spkt <hntxuanit15spkt@gmail.com>
2017-11-16 09:22:12 +0700

    a

--- WebApplication1/Models/NGUOIDUNG.cs
+++ WebApplication1/Models/NGUOIDUNG.cs
@@ -9,22 +9,40 @@ using WebApplication1.Models;
 namespace WebApplication1.Models
 {
   public class NGUOIDUNG
+  {
+    private DataRow dataRow;
+
+    public NGUOIDUNG(DataRow dataRow)
     {
-        public string Ho { get; set; }
-        public string TenLot { get; set; }
-        public string Ten { get; set; }
-        public bool GioiTinh { get; set; }
-        public string DiaChi { get; set; }
-        public string SoDienThoai { get; set; }
-        public int MaNguoiDung { get; set; }
-        public string Email { get; set; }
-        public int MaLoaiNguoiDung { get; set; }
-        public string TaiKhoan { get; set; }
-        public string MatKhau { get; set; }
-        public bool TrangThai { get; set; }
-        public virtual ICollection<GIOHANG> GIOHANGs { get; set; }
-        public virtual LOAINGUOIDUNG LOAINGUOIDUNG { get; set; }
-        public virtual ICollection<PHIEUNHAP> PHIEUNHAPs { get; set; }
-        public virtual ICollection<QUYENHANNGUOIDUNG> QUYENHANNGUOIDUNGs { get; set; }
+      this.Ho = dataRow["Ho"].ToString();
+      this.TenLot = dataRow["TenLot"].ToString();
+      this.Ten = dataRow["Ten"].ToString();
+      this.GioiTinh = (bool)dataRow["GioiTinh"];
+      this.DiaChi = dataRow["DiaChi"].ToString();
+      this.SoDienThoai = dataRow["SoDienThoai"].ToString();
+      this.MaNguoiDung = (int)dataRow["MaNguoiDung"];
+      this.Email = dataRow["Email"].ToString();
+      this.MaLoaiNguoiDung = (int)dataRow["MaLoaiNguoiDung"];
+      this.TaiKhoan = dataRow["TaiKhoan"].ToString();
+      this.MatKhau = dataRow["MatKhau"].ToString();
+      this.TrangThai = (bool)dataRow["TrangThai"];
     }
+
+    public string Ho { get; set; }
+    public string TenLot { get; set; }
+    public string Ten { get; set; }
+    public bool GioiTinh { get; set; }
+    public string DiaChi { get; set; }
+    public string SoDienThoai { get; set; }
+    public int MaNguoiDung { get; set; }
+    public string Email { get; set; }
+    public int MaLoaiNguoiDung { get; set; }
+    public string TaiKhoan { get; set; }
+    public string MatKhau { get; set; }
+    public bool TrangThai { get; set; }
+    public virtual ICollection<GIOHANG> GIOHANGs { get; set; }
+    public virtual LOAINGUOIDUNG LOAINGUOIDUNG { get; set; }
+    public virtual ICollection<PHIEUNHAP> PHIEUNHAPs { get; set; }
+    public virtual ICollection<QUYENHANNGUOIDUNG> QUYENHANNGUOIDUNGs { get; set; }
+  }
 }
